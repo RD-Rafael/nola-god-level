a8189b80b4cc624c947d4dbf3d06795099f1a512
Rafael Rocha <efp.rafael.rd@gmail.com>
2025-10-29 20:49:23 -0300

    add requests to backend in frontend

--- frontend/app.py
+++ frontend/app.py
@@ -2,8 +2,16 @@
 import psycopg2
 import pandas as pd
 import os
+import requests
 
+api_url = "http://godlevel-backend:8000"
+params = {
+    "page" : 1,
+    "pageSize" : 100
+}
 
-st.title('God Level')
+response = requests.get(api_url + "/sales", params=params)
+
+st.title(response.json())
 
 
