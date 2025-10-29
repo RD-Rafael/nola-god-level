6d83f4b0d7da0d364b2a8d601de61baaa8a4bd14
Rafael Rocha <efp.rafael.rd@gmail.com>
2025-10-29 20:48:25 -0300

    add models initialization file

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,13 +1,33 @@
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.types import  TIMESTAMP, DECIMAL
 from db_setup import Base
 
 
 class Customer(Base):
-    __tablename__ = 'Customers'
+    __tablename__ = 'customers'
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    customer_name = Column(String)
     email = Column(String)
 
     def __repr__(self):
         return f"Customer(ie = {self.id}, name = '{self.name}')"
+    
+
+class Sale(Base):
+    __tablename__ = 'sales'
+    id = Column(Integer, primary_key =True)
+    store_id = Column(Integer)
+    sub_brand_id = Column(Integer)
+    customer_id = Column(Integer)
+    channel_id = Column(Integer)
+
+    cod_sale1 = Column(String)
+    created_at = Column(TIMESTAMP)
+
+    total_amount_items = Column(DECIMAL)
+    total_amount = Column(DECIMAL)
+    value_paid = Column(DECIMAL)
+    
+    production_seconds = Column(Integer)
+    delivery_seconds = Column(Integer)
